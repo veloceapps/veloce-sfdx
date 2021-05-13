55ae72b2aaa3a28b2d7350d38c6a64a3ac0a49a1
vkorehov <vladislavs.korehovs@veloceapps.com>
2021-05-13 11:36:41 +0300

    allow overriding pricelist with passed in args

--- src/commands/veloce/packui.ts
+++ src/commands/veloce/packui.ts
@@ -24,6 +24,7 @@ export default class Org extends SfdxCommand {
 
   protected static flagsConfig = {
     modelname: flags.string({char: 'n', description: messages.getMessage('modelnameFlagDescription'), required: true}),
+    pricelistiid: flags.string({char: 'P', description: messages.getMessage('pricelistidFlagDescription'), required: false}),
     inputdir: flags.string({char: 'i', description: messages.getMessage('inputdirFlagDescription'), required: true}),
     outputfile: flags.string({char: 'o', description: messages.getMessage('outputfileFlagDescription'), required: true}),
     idmap: flags.string({char: 'I', description: messages.getMessage('idmapFlagDescription'), required: true})
@@ -55,9 +56,21 @@ export default class Org extends SfdxCommand {
     const input = JSON.parse(inputString);
     for (const metadata of input) {
       if (metadata['priceList']) {
-        if (idmap[metadata['priceList']]) {
-          this.ux.log(`${metadata['priceList']} => ${idmap[metadata['priceList']]}`);
-          metadata['priceList'] = idmap[metadata['priceList']];
+        if (this.flags.pricelistiid) { // Prioritize passed in --pricelistid
+          if (idmap[this.flags.pricelistiid]) {
+            this.ux.log(`${metadata['priceList']} => ${idmap[this.flags.pricelistiid]}`);
+            metadata['priceList'] = idmap[metadata['priceList']];
+          } else {
+            this.ux.log(`${metadata['priceList']} => ${this.flags.pricelistiid}`);
+            metadata['priceList'] = this.flags.pricelistiid;
+          }
+        } else {
+          if (idmap[metadata['priceList']]) {
+            this.ux.log(`${metadata['priceList']} => ${idmap[metadata['priceList']]}`);
+            metadata['priceList'] = idmap[metadata['priceList']];
+          } else {
+            this.ux.log(`Keeping existing pricelistId: ${metadata['priceList']} since no --pricelistid specified!`);
+          }
         }
       }
       for (const section of metadata['sections']) {
