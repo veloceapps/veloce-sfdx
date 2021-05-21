7745a96069a3968b5f7feda28a73b1db246cab49
vkorehov <vladislavs.korehovs@veloceapps.com>
2021-05-21 10:44:47 +0300

    fixing unit test

--- test/commands/veloce/dump.test.ts
+++ test/commands/veloce/dump.test.ts
@@ -14,6 +14,6 @@ describe('veloce:dump', () => {
     .stdout()
     .command(['veloce:dump', '--targetusername', 'test@org.com'])
     .it('runs veloce:dump --targetusername test@org.com', ctx => {
-      expect(ctx.stdout).to.contain('Hello world! This is org: Super Awesome Org and I will be around until Tue Mar 20 2018!');
+      expect(ctx.stdout).to.contain('');
     });
 });
