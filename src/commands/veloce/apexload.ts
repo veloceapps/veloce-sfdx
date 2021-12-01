312ea0d999b0c40babaf417c607534a4a545aed9
vkorehov <vladislavs.korehovs@veloceapps.com>
2021-12-01 15:38:41 +0200

    validate sf id checksum

--- src/commands/veloce/apexload.ts
+++ src/commands/veloce/apexload.ts
@@ -18,7 +18,37 @@ Messages.importMessagesDirectory(__dirname);
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
 const messages = Messages.loadMessages('veloce-sfdx', 'apexload');
-const SalesforceIdRegex = new RegExp('^[a-zA-Z0-9]{15}|[a-zA-Z0-9]{18}$')
+const salesforceIdRegex = new RegExp('^[a-zA-Z0-9]{18}$');
+
+const validSFID = (input: string): boolean => {
+  // https://stackoverflow.com/a/29299786/1333724
+  if (!salesforceIdRegex.test(input)) {
+    return false;
+  }
+  const parts = [input.substr(0, 5),
+    input.substr(5, 5),
+    input.substr(10, 5)];
+  const chars: number[] = [0, 0, 0];
+  for (let j = 0; j < parts.length; j++) {
+    const word = parts[j];
+    for (let i = 0; i < word.length; i++) {
+      const char = word.charCodeAt(i);
+      if (char >= 65 && char <= 90) {
+        chars[j] += 1 << i;
+      }
+    }
+  }
+  for (let i = 0; i < chars.length; i++) {
+    const c = chars[i];
+    if (c <= 25) {
+      chars[i] = c + 65;
+    } else {
+      chars[i] = c - 25 + 48;
+    }
+  }
+
+  return (String.fromCharCode(chars[0], chars[1], chars[2])) === input.substr(15, 3);
+};
 
 export default class Org extends SfdxCommand {
 
@@ -71,13 +101,13 @@ export default class Org extends SfdxCommand {
 
   public async run(): Promise<AnyJson> {
     if (this.flags.boolfields) {
-      this.ux.warn(`-B or --boolfields arg is DEPRECATED, types are inferred automatically! please remove`);
+      this.ux.warn('-B or --boolfields arg is DEPRECATED, types are inferred automatically! please remove');
     }
     if (this.flags.datefields) {
-      this.ux.warn(`-D or --datefields arg is DEPRECATED, types are inferred automatically! please remove`);
+      this.ux.warn('-D or --datefields arg is DEPRECATED, types are inferred automatically! please remove');
     }
     if (this.flags.numericfields) {
-      this.ux.warn(`-N or --numericfields arg is DEPRECATED, types are inferred automatically! please remove`);
+      this.ux.warn('-N or --numericfields arg is DEPRECATED, types are inferred automatically! please remove');
     }
 
     let ok = true;
@@ -191,8 +221,8 @@ WHERE EntityDefinition.QualifiedApiName IN ('${this.flags.sobjecttype}') ORDER B
           } else if (numericfields.includes(k)) {
             fields.push(`${upsert ? '' : 'o.'}${k}=${s}`);
           } else {
-            if (this.flags.strict && SalesforceIdRegex.test(s) && k !== extId) {
-              idsToValidate.push(s)
+            if (this.flags.strict && k !== extId && validSFID(s) ) {
+              idsToValidate.push(s);
             }
             fields.push(`${upsert ? '' : 'o.'}${k}='${s
               .replaceAll('\\', '\\\\')
